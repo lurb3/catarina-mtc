bd0e0ca7c7bfb1ae7b1288729607ad026795554b
Gustavo Monteiro <gustavo.gigante.s@gmail.com>
2026-05-10 11:44:54 +0100

    try fix

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -7,7 +7,7 @@
 import { NextStudio } from "next-sanity/studio";
 import config from "../../../../sanity.config";
 
-export const dynamic = "force-dynamic";
+export const dynamic = "force-static";
 export { metadata, viewport } from "next-sanity/studio";
 
 export default function StudioPage() {
